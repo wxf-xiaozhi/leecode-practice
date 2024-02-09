2ed515825a421eabcf2870e37eeaa0fbe79dc4e9
983053245@qq.com <983053245@qq.com>
2024-02-09 17:31:35 +0800

    26题

--- src/com/sxnd/leecode/InterviewTop150.java
+++ src/com/sxnd/leecode/InterviewTop150.java
@@ -1,7 +1,6 @@
 package com.sxnd.leecode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 面试经典150题
@@ -18,10 +17,62 @@ public class InterviewTop150 {
      * @param B
      * @param n
      */
-    public void merge(int[] A, int m, int[] B, int n) {
+    public static void merge(int[] A, int m, int[] B, int n) {
 //        输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
 //        输出：[1,2,2,3,5,6]
+        int[] merge = new int[m+n];
+        int p1 = 0,p2 =0;
+        int cur = 0;
+        while (p1< m || p2 < n){
+            if(p1 == m){
+                cur = B[p2];
+                p2++;
+            }else if(p2 == m){
+                cur = A[p1];
+                p1++;
+            }else if(A[p1] <= B[p2]){
+                cur = A[p1];
+                p1++;
+            }else{
+                cur = B[p2];
+                p2++;
+            }
+            merge[p1+p2-1] = cur;
 
+        }
+        for (int i = 0; i < merge.length; i++) {
+            A[i] = merge[i];
+        }
+    }
+
+    /**
+     *
+     * 26. 删除有序数组中的重复项
+     * @param nums
+     * @return
+     */
+    public static  int removeDuplicates(int[] nums) {
+//        Set<Integer> mySet = new LinkedHashSet<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            mySet.add(nums[i]);
+//        }
+//        int[] a = new int[mySet.size()];
+//        Iterator<Integer> iterator = mySet.iterator();
+//        int index = 0;
+//        while (iterator.hasNext()){
+//            a[index] = iterator.next();
+//            index++;
+//        }
+//        nums = a;
+//        return nums.length;
+        int p1 = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i-1] != nums[i]){
+                nums[p1] = nums[i];
+                p1++;
+            }
+        }
+        return p1;
     }
 
     /**
@@ -53,7 +104,10 @@ public class InterviewTop150 {
     }
 
     public static void main(String[] args) {
-        int[] a ={3,2,2,3};
-        removeElement(a,3);
+//        int[] a ={3,2,2,3};
+//        removeElement(a,3);
+
+        int[] a ={1,1,2};
+        removeDuplicates(a);
     }
 }
